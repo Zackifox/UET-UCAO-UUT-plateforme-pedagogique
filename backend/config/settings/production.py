411065f2cf490df19faabece81645991e72fe087
Zackifox <davidngomdjibaye@gmail.com>
2026-04-17 16:13:07 +0000

    Fix gunicorn config for Render3

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,18 +1,16 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 CORS_ALLOWED_ORIGINS = config(
